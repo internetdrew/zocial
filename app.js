049b17deddf0ec2436d0be2f41fdd8ce6e8b4b19
Andrew Rowley <andrew@internetdrew.com>
2022-10-06 12:41:27 -0400

    Change icon pack to fix indexing issues

--- app.js
+++ app.js
@@ -2,14 +2,29 @@
 
 const menu = document.getElementById('menu');
 const menuItems = document.querySelectorAll('.menu-item');
-console.log(menuItems);
+const notificationPopup = document.getElementById('notifications-popup');
 
 const deactivateMenuItems = function () {
   menuItems.forEach(item => item.classList.remove('active'));
 };
 
+const showNotificationPopup = function () {
+  console.log(notificationPopup.parentElement);
+  notificationPopup.style.display = 'block';
+  console.log(notificationPopup);
+};
+
+// Event Listeners
 menu.addEventListener('click', e => {
   const menuItem = e.target.closest('.menu-item');
   deactivateMenuItems();
   menuItem.classList.add('active');
+
+  if (menuItem.id === 'notifications-btn') {
+    showNotificationPopup();
+  }
+});
+
+document.addEventListener('click', e => {
+  console.dir(e.target);
 });
